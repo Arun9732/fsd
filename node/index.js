83974a397d4ca5209b9e255ba1cebfa54269994a
Arun kumar <ak9015748750@gmail.com>
2026-02-12 14:27:48 +0530

    node

---
+++ node/index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const myserver = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("ABES ENGINEERING COLLEGE");
+  } else if (req.url == "/about") {
+    res.end("This is class of CSE");
+  }else if (req.url == "/contact") {
+    res.end("97*******");
+  }
+   else {
+    res.end("404");
+  }
+});
+
+myserver.listen(8002, () => console.log("Server started"));
