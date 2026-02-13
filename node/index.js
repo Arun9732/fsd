0c3cbc0f619c0a3ae5250c3c5541dee0dd588c13
Arun kumar <ak9015748750@gmail.com>
2026-02-13 12:13:02 +0530

    Adding file

--- node/index.js
+++ node/index.js
@@ -1,16 +1,41 @@
-const http = require("http");
+// const http = require("http");
 
-const myserver = http.createServer((req, res) => {
-  if (req.url == "/") {
-    res.end(`<img src=""></img>`);
-  } else if (req.url == "/about") {
-    res.end("This is class of CSE");
-  }else if (req.url == "/contact") {
-    res.end("97*******");
-  }
-   else {
-    res.end("404");
+// const myserver = http.createServer((req, res) => {
+//   if (req.url == "/") {
+//     res.end(`<img src=""></img>`);
+//   } else if (req.url == "/about") {
+//     res.end("This is class of CSE");
+//   }else if (req.url == "/contact") {
+//     res.end("97*******");
+//   }
+//    else {
+//     res.end("404");
+//   }
+// });
+
+// myserver.listen(8002, () => console.log("Server started"));
+
+// file by Sync method
+const fs = require("fs");
+// fs.writeFile("./cse.txt","hi we are here",(err)=>{
+//   if (err) {
+//      console.log("error", err);
+//    } else{
+//     console.log("created");
+//    }
+
+// });
+
+//[Read using if {callback}]
+fs.readFile("./cse.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log("error", err);
+  } else {
+    console.log(result);
   }
 });
 
-myserver.listen(8002, () => console.log("Server started"));
+// const result=fs.readFileSync("./cse.txt","utf-8");
+// console.log(result);
+
+fs.appendFileSync("./cse.txt",' coder');
