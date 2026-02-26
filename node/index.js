bcdf91ee0f8aef1ea008ca670fe6068cd5b850e1
Arun kumar <ak9015748750@gmail.com>
2026-02-26 14:10:12 +0530

    node

--- node/index.js
+++ node/index.js
@@ -16,7 +16,7 @@
 // myserver.listen(8002, () => console.log("Server started"));
 
 // file by Sync method
-const fs = require("fs");
+// const fs = require("fs");
 // fs.writeFile("./cse.txt","hi we are here",(err)=>{
 //   if (err) {
 //      console.log("error", err);
@@ -27,13 +27,13 @@ const fs = require("fs");
 // });
 
 //[Read using if {callback}]
-fs.readFile("./cse.txt", "utf-8", (err, result) => {
-  if (err) {
-    console.log("error", err);
-  } else {
-    console.log(result);
-  }
-});
+// fs.readFile("./cse.txt", "utf-8", (err, result) => {
+//   if (err) {
+//     console.log("error", err);
+//   } else {
+//     console.log(result);
+//   }
+// });
 
 // const result=fs.readFileSync("./cse.txt","utf-8");
 // console.log(result);
@@ -44,4 +44,31 @@ fs.readFile("./cse.txt", "utf-8", (err, result) => {
 
 //{Creating new File}
 
-fs.cpSync("./cse.txt","./b.txt");
+// fs.cpSync("./cse.txt","./b.txt");
+
+
+
+/*<..............................{ [L-3] OS Learning Class }............................>*/
+
+// const os=require('os');
+// console.log("platform",os.platform());
+// console.log("userinfo",os.userInfo());
+// console.log("CPU",os.arch());
+// console.log("free memory",os.freemem());
+// console.log("total memory",os.totalmem());
+// console.log("uptime",os.uptime());
+// console.log("home dir",os.homedir());
+// console.log("host name",os.hostname());
+
+
+
+/*<..............................{ [L-3.2]  }............................>*/
+
+const http=require('http');
+const fs=require('fs');
+const home=fs.readFileSync('./a.html')
+const myserver=http.createServer((req,res)=>{
+  res.end(home);  
+})
+
+myserver.listen(8000,()=>console.log("server is run"));
